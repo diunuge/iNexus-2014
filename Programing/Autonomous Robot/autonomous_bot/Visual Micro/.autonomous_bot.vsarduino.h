98b5df70fee1e9b75f49eaf88998d6e1abb7c31f
diunuge <diunuge.10@cse.mrt.ac.lk>
2013-09-17 22:08:00 +0530

    Initialization of codes

---
+++ Programing/Autonomous Robot/autonomous_bot/Visual Micro/.autonomous_bot.vsarduino.h
@@ -0,0 +1,31 @@
+//Board = Arduino Mega 2560
+#define ARDUINO 23
+#define __AVR_ATmega2560__
+#define F_CPU 16000000L
+#define __AVR__
+#define __cplusplus
+#define __attribute__(x)
+#define __inline__
+#define __asm__(x)
+#define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
+#define __inline__
+#define __asm__ 
+#define __volatile__
+#define __builtin_va_list
+#define __builtin_va_start
+#define __builtin_va_end
+#define __DOXYGEN__
+#define prog_void
+#define PGM_VOID_P int
+#define NOINLINE __attribute__((noinline))
+
+typedef unsigned char byte;
+extern "C" void __cxa_pure_virtual() {}
+
+//already defined in arduno.h
+//already defined in arduno.h
+
+#include "L:\Softwares\Programming Softwares\Electronics\Arduino Setup\arduino-0023\hardware\arduino\cores\arduino\wprogram.h"
+#include "C:\Users\D\Documents\Arduino\autonomous_bot\autonomous_bot.pde"
